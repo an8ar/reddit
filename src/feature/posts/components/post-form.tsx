049cc357d4713788eaef5f03620074620876db1e
an8ar <annssarr@gmail.com>
2024-06-04 11:46:03 +0500

    add: form for a post, with a dynamically chaining fields

--- src/feature/posts/components/post-form.tsx
+++ src/feature/posts/components/post-form.tsx
@@ -14,23 +14,29 @@ import {
 import { FormProvider, RHFTextArea } from "~/components/hook-form";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Post } from "../types";
+import { FormType, Post } from "../types";
 import * as Yup from "yup";
 import { Button } from "~/components/ui/button";
 import { useAppDispatch } from "~/store/hooks";
 import { addPost } from "../posts-slice";
+import Image from "next/image";
+import { UploadFile } from "~/components/upload-file";
+import { FileUploadArea } from "~/components/upload-file-area";
+
 type FormValuesProps = Omit<Post, "id" | "createdAt">;
 
 const CreatePostSchema = Yup.object().shape({
   title: Yup.string().required("Please fill out this field."),
   imageUrls: Yup.array().of(Yup.string().required()).optional(),
+  text: Yup.string().optional(),
 });
 
 interface Props {
   closeModal: () => void;
+  type: FormType;
 }
 
-export function PostImageForm({ closeModal }: Props) {
+export function PostForm({ closeModal, type }: Props) {
   const [photos, setPhotos] = useState<string[]>([]);
   const dispatch = useAppDispatch();
 
@@ -81,44 +87,52 @@ export function PostImageForm({ closeModal }: Props) {
 
   return (
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-      <section className="flex flex-col gap-2">
+      <section className="flex flex-col gap-2 mt-2">
         <RHFTextArea
           name="title"
           placeholder="Title"
           className="min-h-10"
           maxLength={300}
         />
-        {photos.length <= 0 ? (
-          <FileUploadArea handleFileChange={handleFileChange} />
+        {type == "image" ? (
+          <>
+            {photos.length <= 0 ? (
+              <FileUploadArea handleFileChange={handleFileChange} />
+            ) : (
+              <div className="backdrop-blur-2xl bg-slate-200 rounded-2xl overflow-hidden h-100 group">
+                <Carousel className="w-full flex justify-between items-center gap-2 px-8 relative">
+                  <div className="absolute top-2 left-4 z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                    <UploadFile handleFileChange={handleFileChange} />
+                  </div>
+                  <div>
+                    <CarouselPrevious />
+                  </div>
+                  <CarouselContent>
+                    {photos.map((photo, index) => (
+                      <CarouselItem key={index}>
+                        <div className=" max-h-72 flex justify-center items-center">
+                          <Image
+                            className="object-scale-down"
+                            src={photo}
+                            alt="Sheraton Hotel"
+                            width={200}
+                            height={200}
+                          />
+                        </div>
+                      </CarouselItem>
+                    ))}
+                  </CarouselContent>
+                  <div>
+                    <CarouselNext />
+                  </div>
+                </Carousel>
+              </div>
+            )}
+          </>
         ) : (
-          <div className="backdrop-blur-2xl bg-slate-200 rounded-2xl overflow-hidden h-100 group">
-            <Carousel className="w-full flex justify-between items-center gap-2 px-8 relative">
-              <div className="absolute top-2 left-4 z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <UploadPhoto handleFileChange={handleFileChange} />
-              </div>
-              <div>
-                <CarouselPrevious />
-              </div>
-              <CarouselContent>
-                {photos.map((photo, index) => (
-                  <CarouselItem key={index}>
-                    <div className=" max-h-72 flex justify-center items-center">
-                      <img
-                        className="object-scale-down"
-                        src={photo}
-                        alt="Sheraton Hotel"
-                        width={200}
-                      />
-                    </div>
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-              <div>
-                <CarouselNext />
-              </div>
-            </Carousel>
-          </div>
+          <RHFTextArea name="text" placeholder="Body" className="h-40" />
         )}
+
         <div className="flex gap-5 justify-end">
           <Button className="rounded-full bg-blue-800" type="button">
             Save draft
@@ -131,53 +145,3 @@ export function PostImageForm({ closeModal }: Props) {
     </FormProvider>
   );
 }
-
-interface UploadPhotoProps {
-  handleFileChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}
-
-const UploadPhoto = ({ handleFileChange }: UploadPhotoProps) => {
-  return (
-    <div className=" ">
-      <Label
-        htmlFor="inputPhoto"
-        className="inset-0  px-2 py-3 rounded-full hover:cursor-pointer flex items-center gap-2 bg-stone-400 text-white"
-      >
-        <Icon icon="carbon:image" />
-        <span>Add</span>
-      </Label>
-      <Input
-        type="file"
-        id="inputPhoto"
-        accept="image/*"
-        onChange={handleFileChange}
-        multiple
-        hidden
-        className="file-selector-button-none hidden"
-      />
-    </div>
-  );
-};
-
-const FileUploadArea = ({ handleFileChange }: UploadPhotoProps) => {
-  return (
-    <div className="relative w-full aspect-video rounded-2xl border-2 border-dotted border-slate-600  hover:cursor-pointer h-40 dot">
-      <Label
-        htmlFor="inputImage"
-        className="absolute inset-0 flex gap-4 items-center justify-center"
-      >
-        <Icon icon="ph:plus-thin" />
-        <span className="text-lg">Add photo</span>
-      </Label>
-      <Input
-        type="file"
-        id="inputImage"
-        accept="image/*"
-        onChange={handleFileChange}
-        multiple
-        hidden
-        className="file-selector-button-none hidden"
-      />
-    </div>
-  );
-};
