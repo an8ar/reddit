c334ff345b551665abcc7346164eb4640252e727
an8ar <annssarr@gmail.com>
2024-06-13 16:02:57 +0500

    add: custom-cached selected posts according to params

--- src/feature/posts/components/post-list.tsx
+++ src/feature/posts/components/post-list.tsx
@@ -5,11 +5,19 @@ import { Post } from "./post";
 import TimeAgo from "javascript-time-ago";
 
 import en from "javascript-time-ago/locale/en";
+import { useSelector } from "react-redux";
+import { selectSortedPosts } from "../selector";
+import { RootState } from "~/store";
 TimeAgo.addDefaultLocale(en);
 
-export function PostList() {
-  const posts = useAppSelector((state) => state.postsSlice.posts);
+interface Props {
+  searchParams: { [key: string]: string | string[] | undefined };
+}
 
+export function PostList({ searchParams }: Props) {
+  const posts = useSelector((state: RootState) =>
+    selectSortedPosts(state, searchParams)
+  );
   return (
     <div>
       {posts.length > 0 ? (
