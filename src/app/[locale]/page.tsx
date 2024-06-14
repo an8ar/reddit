5dfd183bcc2565b0af3de35d5bd3fdeef802e271
an8ar <annssarr@gmail.com>
2024-06-14 14:59:53 +0500

    add: i18n

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,6 +6,8 @@ export default function Home({
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
+  const t = useTranslations('Index');
+
   return (
     <main className="flex-1 flex flex-col gap-10">
       <header className=" border-b py-2">
