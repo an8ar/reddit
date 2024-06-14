b785955e79676d271abc0d8bb75456d5c16f599e
an8ar <annssarr@gmail.com>
2024-06-14 17:07:32 +0500

    add: internationalization text

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -7,8 +7,6 @@ export default function Home({
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  const t = useTranslations('Index');
-
   return (
     <main className="flex-1 flex flex-col gap-10">
       <header className=" border-b py-2 flex gap-4">
