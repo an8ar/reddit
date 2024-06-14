366b4f5022b64520a6d3a603f020bb9a9e2593cc
an8ar <annssarr@gmail.com>
2024-06-14 14:42:44 +0500

    refactor: selector logic

--- src/feature/posts/selector.ts
+++ src/feature/posts/selector.ts
@@ -1,55 +1,49 @@
-import { createSelector } from "@reduxjs/toolkit";
-import { RootState } from "~/store";
-import dayjs from "dayjs";
+import { createSelector } from '@reduxjs/toolkit';
+import { RootState } from '~/store';
+import dayjs from 'dayjs';
+import { Post } from './types';
+
+const sortByName = (posts: Post[], order: 'asc' | 'desc') => {
+  return posts.sort((a, b) => {
+    const comparison = a.title.localeCompare(b.title);
+    return order === 'asc' ? comparison : -comparison;
+  });
+};
+
+const sortByDate = (posts: Post[], order: 'asc' | 'desc') => {
+  return posts.sort((a, b) => {
+    const comparison = dayjs(a.createdAt).isBefore(dayjs(b.createdAt)) ? -1 : 1;
+    return order === 'asc' ? comparison : -comparison;
+  });
+};
 
 const selectPosts = (state: RootState) => state.postsSlice.posts;
+
 const selectSearchParams = (
   state: RootState,
-  searchParams: { [key: string]: string | string[] | undefined }
+  searchParams: { [key: string]: string | string[] | undefined },
 ) => searchParams;
+
+const sortingHandlers = {
+  name: sortByName,
+  date: sortByDate,
+};
 
 export const selectSortedPosts = createSelector(
   [selectPosts, selectSearchParams],
   (posts, searchParams) => {
     let filteredPosts = [...posts];
 
-    if (searchParams.sortBy === "name") {
-      if (searchParams.order === "asc") {
-        filteredPosts = filteredPosts.sort((a, b) =>
-          a.title.localeCompare(b.title)
-        );
-      }
-      if (searchParams.order === "desc") {
-        filteredPosts = filteredPosts.sort((a, b) =>
-          b.title.localeCompare(a.title)
-        );
+    const sortBy = searchParams.sortBy as keyof typeof sortingHandlers;
+    const order = searchParams.order as 'asc' | 'desc';
+
+    if (sortBy && order) {
+      const sortHandler = sortingHandlers[sortBy];
+      if (sortHandler) {
+        filteredPosts = sortHandler(filteredPosts, order);
       }
     }
 
-    if (searchParams.sortBy === "date") {
-      if (searchParams.order === "asc") {
-        filteredPosts = filteredPosts.sort((a, b) => {
-          if (dayjs(a.createdAt).isBefore(dayjs(b.createdAt))) {
-            return 1;
-          }
-          if (dayjs(a.createdAt).isAfter(dayjs(b.createdAt))) {
-            return -1;
-          }
-          return 0;
-        });
-      }
-      if (searchParams.order === "desc") {
-        filteredPosts = filteredPosts.sort((a, b) => {
-          if (dayjs(a.createdAt).isBefore(dayjs(b.createdAt))) {
-            return -1;
-          }
-          if (dayjs(a.createdAt).isAfter(dayjs(b.createdAt))) {
-            return 1;
-          }
-          return 0;
-        });
-      }
-    }
     return filteredPosts;
-  }
+  },
 );
