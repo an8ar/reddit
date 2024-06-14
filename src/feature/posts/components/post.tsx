e694d1277d36a13df0552204adf531fcbf4b1fd3
an8ar <annssarr@gmail.com>
2024-06-14 17:07:55 +0500

    add: react time ago with internationalization

--- src/feature/posts/components/post.tsx
+++ src/feature/posts/components/post.tsx
@@ -1,18 +1,22 @@
-import React from "react";
-import { Post as IPost } from "../types";
-import Image from "next/image";
-import { Interactions } from "~/feature/post-interactions";
-import ReactTimeAgo from "react-time-ago";
+'use client';
+import React from 'react';
+import { Post as IPost } from '../types';
+import Image from 'next/image';
+import { Interactions } from '~/feature/post-interactions';
+import ReactTimeAgo from 'react-time-ago';
+import { useParams } from 'next/navigation';
 
 interface Props extends IPost {}
 
 export function Post({ title, imageUrls, text, linkUrl, createdAt }: Props) {
+  const params = useParams<{ locale: string }>();
+
   return (
     <div className="flex flex-col gap-2 hover:bg-slate-100 rounded-lg px-4 py-2">
       <div className="flex gap-2 text-xs">
         <span className="font-semibold">an8ar</span>
         <span className="text-gray-400">
-          <ReactTimeAgo date={new Date(createdAt)} locale="en-US" />
+          <ReactTimeAgo date={new Date(createdAt)} locale={params.locale} />
         </span>
       </div>
       <div className="text-xl font-semibold">{title}</div>
@@ -30,11 +34,7 @@ export function Post({ title, imageUrls, text, linkUrl, createdAt }: Props) {
       )}
       {text && <p className="text-sm text-gray-600">{text}</p>}
       {linkUrl && (
-        <a
-          href={linkUrl}
-          target="_blank"
-          className="text-blue-700 visited:text-violet-500"
-        >
+        <a href={linkUrl} target="_blank" className="text-blue-700 visited:text-violet-500">
           https://www.w3schools.com
         </a>
       )}
