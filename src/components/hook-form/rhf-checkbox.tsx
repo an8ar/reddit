3e6b336e7bace89d6689e38ba66b7b344792a292
an8ar <annssarr@gmail.com>
2024-06-18 16:58:21 +0500

    refactor: onChange optional field for rhf checkbox

--- src/components/hook-form/rhf-checkbox.tsx
+++ src/components/hook-form/rhf-checkbox.tsx
@@ -1,4 +1,4 @@
-import { useFormContext } from 'react-hook-form';
+import { useController, useFormContext } from 'react-hook-form';
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '~/components/ui/form';
 import { Checkbox } from '~/components/ui/checkbox';
 
@@ -6,23 +6,35 @@ interface RHFCheckboxProps {
   name: string;
   label?: string;
   description?: { icon: JSX.Element; text: string };
+  onChange?: () => void; // Added to allow handling changes
 }
 
-export function RHFCheckbox({ name, label, description }: RHFCheckboxProps) {
+export function RHFCheckbox({ name, label, description, onChange }: RHFCheckboxProps) {
   const { control } = useFormContext();
+
+  const {
+    field: { value, onChange: onFieldChange, onBlur },
+  } = useController({
+    name,
+    control,
+  });
+
+  const handleChange = (checked: boolean) => {
+    onFieldChange(checked);
+
+    if (onChange) {
+      onChange();
+    }
+  };
 
   return (
     <FormField
       control={control}
       name={name}
-      render={({ field }) => (
+      render={() => (
         <FormItem className="flex flex-row items-center space-x-3 space-y-0 rounded-md ">
           <FormControl>
-            <Checkbox
-              checked={field.value}
-              onCheckedChange={field.onChange}
-              onBlur={field.onBlur}
-            />
+            <Checkbox checked={value} onCheckedChange={handleChange} onBlur={onBlur} />
           </FormControl>
           <div className="flex gap-4 items-center">
             {label && <FormLabel className="text-sm">{label}</FormLabel>}
