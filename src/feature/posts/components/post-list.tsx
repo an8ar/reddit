f25b81d74cca5141ac6fedd793f772ad23e89605
an8ar <annssarr@gmail.com>
2024-06-19 18:09:30 +0500

    fix: remove not needed  searchParams interface's field

--- src/feature/posts/components/post-list.tsx
+++ src/feature/posts/components/post-list.tsx
@@ -11,7 +11,7 @@ import ru from 'javascript-time-ago/locale/ru';
 TimeAgo.addDefaultLocale(en);
 TimeAgo.addLocale(ru);
 interface Props {
-  searchParams: { [key: string]: string | string[] | undefined };
+  searchParams: { [key: string]: string | undefined };
 }
 
 export function PostList({ searchParams }: Props) {
