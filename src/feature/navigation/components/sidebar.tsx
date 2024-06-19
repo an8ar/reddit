95724a756a08aada27d7db3c1c33364ba7a9e818
an8ar <annssarr@gmail.com>
2024-06-19 14:41:14 +0500

    add: links to corresponding pages

--- src/feature/navigation/components/sidebar.tsx
+++ src/feature/navigation/components/sidebar.tsx
@@ -7,8 +7,13 @@ import {
   AccordionTrigger,
 } from '~/components/ui/accordion';
 import { useTranslations } from 'next-intl';
+import Link from 'next/link';
 
-export function SideBar() {
+interface Props {
+  locale: string;
+}
+
+export function SideBar({ locale }: Props) {
   const t = useTranslations('SideBar');
 
   const feeds = useMemo(
@@ -16,14 +21,17 @@ export function SideBar() {
       {
         name: t('home'),
         icon: <Icon icon="ic:round-home" className="text-[28px]" />,
+        href: `/${locale}`,
       },
       {
         name: t('popular'),
         icon: <Icon icon="solar:graph-up-linear" className="text-[28px]" />,
+        href: `/${locale}/popular`,
       },
       {
         name: t('all'),
         icon: <Icon icon="f7:chart-bar-circle" className="text-[28px]" />,
+        href: `/${locale}/all`,
       },
     ],
     [],
@@ -33,11 +41,13 @@ export function SideBar() {
     <aside className="px-4 py-2 flex-none w-64 border-r ">
       <ul className="space-y-0">
         {feeds.map((feed) => (
-          <li
-            className="flex space-y-0 gap-2 items-center text-sm  rounded-xl hover:bg-gray-100 py-2 px-2"
-            key={feed.name}
-          >
-            {feed.icon} <span>{feed.name}</span>
+          <li key={feed.name}>
+            <Link
+              href={feed.href}
+              className="flex space-y-0 gap-2 items-center text-sm  rounded-xl hover:bg-gray-100 py-2 px-2"
+            >
+              {feed.icon} <span>{feed.name}</span>
+            </Link>
           </li>
         ))}
       </ul>
