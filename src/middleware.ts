a34b4d8ae66417bb27eb3eb8d26f6c7cb452dfc6
an8ar <annssarr@gmail.com>
2024-06-20 16:25:02 +0500

    refactor: abstraction for middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import createMiddleware from 'next-intl/middleware';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default createMiddleware({
+const handleI18nRouting = createMiddleware({
   locales: ['en', 'ru'],
 
   defaultLocale: 'en',
@@ -9,3 +10,7 @@ export default createMiddleware({
 export const config = {
   matcher: ['/', '/(ru|en)/:path*'],
 };
+export default async function middleware(request: NextRequest) {
+  const response = handleI18nRouting(request);
+  return response;
+}
