8de9ccd3f41adce548c6880c28f775cc8bcaf6bc
an8ar <annssarr@gmail.com>
2024-06-20 17:47:56 +0500

    add: integration of persist into store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,8 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { postsSlice } from "~/feature/posts/posts-slice";
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { postSlice, postReducer } from '~/feature/posts/posts-slice';
+import { FLUSH, PAUSE, PERSIST, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
 const reducers = {
-  [postsSlice.name]: postsSlice.reducer,
+  [postSlice.name]: postReducer,
 };
 
 const combinedReducers = combineReducers(reducers);
@@ -10,14 +11,21 @@ export const makeStore = () => {
     reducer: combinedReducers,
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware({
-        serializableCheck: {},
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
       }),
   });
 };
 
 // Infer the type of makeStore
+// Infer the type of makeStore
 export type AppStore = ReturnType<typeof makeStore>;
 
+export const makePersistor = (store: AppStore) => persistStore(store);
+
+export type Persistor = ReturnType<typeof makePersistor>;
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore["getState"]>;
-export type AppDispatch = AppStore["dispatch"];
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
