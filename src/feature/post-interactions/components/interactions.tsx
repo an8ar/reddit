5fe0ec1ee5977cbe66315f0bebf5e801e43949a3
an8ar <annssarr@gmail.com>
2024-06-21 16:35:33 +0500

    add: interactions

--- src/feature/post-interactions/components/interactions.tsx
+++ src/feature/post-interactions/components/interactions.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-import { VotePanel } from "./vote-panel";
+import React from 'react';
+import { VoteButton } from './vote-button';
+import { Comment } from './comment';
+
 export function Interactions() {
   return (
-    <div>
-      <VotePanel />
+    <div className="flex gap-2">
+      <VoteButton />
+      <Comment />
     </div>
   );
 }
--- src/feature/post-interactions/index.ts
+++ src/feature/post-interactions/index.ts
@@ -1,3 +1,3 @@
-export * from "./post-interaction-slice";
-export * from "./components/vote-panel";
-export * from "./components/interactions";
+export * from './post-interaction-slice';
+export * from './components/vote-button';
+export * from './components/interactions';
