32fe6865d6f07c6d966c2a66abd2039e003b4a5f
an8ar <annssarr@gmail.com>
2024-06-21 17:15:47 +0500

    add: use of reducers

--- src/feature/post-interactions/components/vote-button.tsx
+++ src/feature/post-interactions/components/vote-button.tsx
@@ -1,24 +1,42 @@
 import { Icon } from '@iconify/react/dist/iconify.js';
 import React from 'react';
 import { Button } from '~/components/ui/button';
+import { upVote, downVote } from '~/feature/posts/posts-slice';
+import { useAppDispatch } from '~/store/hooks';
+interface Props {
+  voteCount: number;
+  postId: string;
+}
 
-export function VoteButton() {
+export function VoteButton({ voteCount, postId }: Props) {
+  const dispatch = useAppDispatch();
+
+  const handleUpVote = () => {
+    dispatch(upVote({ postId }));
+  };
+
+  const handleDownVote = () => {
+    dispatch(downVote({ postId }));
+  };
+
   return (
     <div className="flex items-center   bg-slate-200 rounded-full ">
       <Button
         className="p-2 hover:bg-slate-300 rounded-full hover:text-red-400"
         size="icon"
         variant={'ghost'}
+        onClick={handleUpVote}
       >
         <Icon icon="mynaui:fat-arrow-up" className=" size-4" />
       </Button>
 
-      <span>111</span>
+      <span>{voteCount}</span>
 
       <Button
         className="p-2 hover:bg-slate-300 rounded-full hover:text-blue-600"
         size="icon"
         variant={'ghost'}
+        onClick={handleDownVote}
       >
         <Icon icon="mynaui:fat-arrow-down" className=" size-4 " />
       </Button>
