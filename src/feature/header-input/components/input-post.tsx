d9d1ff01f4082929f55e18f259e60ca8f2a862ef
an8ar <annssarr@gmail.com>
2024-06-24 15:30:54 +0500

    add: input's posts components

---
+++ src/feature/header-input/components/input-post.tsx
@@ -0,0 +1,48 @@
+'use client';
+import React from 'react';
+import { Post as IPost } from '~/feature/posts/types';
+import Image from 'next/image';
+import { usePathname, useRouter } from 'next/navigation';
+
+interface Props extends IPost {}
+
+export function InputPost({ title, imageUrls, text, linkUrl, id }: Props) {
+  const router = useRouter();
+
+  const pathname = usePathname();
+
+  const handleClick = () => {
+    router.push(`${pathname}/post/${id}`);
+  };
+  return (
+    <div
+      className="flex justify-between gap-1 hover:bg-slate-100  px-4 py-2 hover:cursor-pointer"
+      onClick={handleClick}
+    >
+      <div>
+        <div className="text-sm font-semibold">{title}</div>
+        {text && <p className="text-xs text-gray-600">{text}</p>}
+
+        <span className="font-semibold text-xs rounded-2xl bg-slate-200 px-1">an8ar</span>
+
+        {linkUrl && (
+          <a href={linkUrl} target="_blank" className="text-blue-700 visited:text-violet-500">
+            https://www.w3schools.com
+          </a>
+        )}
+      </div>
+
+      <div>
+        {imageUrls && (
+          <Image
+            src={imageUrls[0]}
+            className=" rounded-lg object-fit"
+            alt={title}
+            width={100}
+            height={100}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
