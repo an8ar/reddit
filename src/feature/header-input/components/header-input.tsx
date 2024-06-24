1d7812d4d4f37e126d38b12d48edc062de390797
an8ar <annssarr@gmail.com>
2024-06-24 16:05:55 +0500

    refactor: remove unused variables

--- src/feature/header-input/components/header-input.tsx
+++ src/feature/header-input/components/header-input.tsx
@@ -7,37 +7,15 @@ import { useAppSelector } from '~/store/hooks';
 import { InputPost } from './input-post';
 
 export function HeaderInput() {
-  const router = useRouter();
-
-  const [value, setValue] = useState('');
-
   const [isVisible, setIsVisible] = useState(false);
 
   const t = useTranslations('Header');
-
-  const pathname = usePathname();
-
-  const searchParams = useSearchParams();
 
   const inputRef = useRef<HTMLInputElement>(null);
 
   const dropdownRef = useRef<HTMLDivElement>(null);
 
-  const params = new URLSearchParams(searchParams.toString());
-
   const posts = useAppSelector((state) => state.postSlice.posts);
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-
-    if (event.target.value === '') {
-      params.delete('title');
-    } else {
-      params.set('title', event.target.value);
-    }
-
-    router.replace(`${pathname}?${params.toString()}`);
-  };
 
   const handleFocus = () => {
     setIsVisible(true);
@@ -53,6 +31,7 @@ export function HeaderInput() {
       setIsVisible(false);
     }
   };
+
   useEffect(() => {
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
@@ -61,22 +40,20 @@ export function HeaderInput() {
   }, []);
 
   return (
-    <div className="relative w-full ">
+    <div className="relative w-full">
       <Input
         ref={inputRef}
         placeholder={t('input')}
-        value={value}
-        className="max-w-xl ml-40  bg-slate-200/65 hover:bg-slate-200 relative z-10 rounded-full focus-visible:ring-0 focus-visible:ring-offset-0"
-        onChange={handleInputChange}
+        className="max-w-xl ml-40 bg-slate-200/65 hover:bg-slate-200 relative z-10 rounded-full focus-visible:ring-0 focus-visible:ring-offset-0"
         onFocus={handleFocus}
       />
       {isVisible && (
         <div
           ref={dropdownRef}
-          className="bg-white shadow-lg rounded-lg mt-2 absolute top-0 pt-12 w-full max-w-xl ml-40 "
+          className="bg-white shadow-lg rounded-lg mt-2 absolute top-0 pt-12 w-full max-w-xl ml-40"
         >
           {posts.slice(0, 9).map((post) => (
-            <InputPost key={post.id} {...post} />
+            <InputPost key={post.id} {...post} setIsVisible={setIsVisible} />
           ))}
         </div>
       )}
