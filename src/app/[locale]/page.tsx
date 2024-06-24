cadfc766cb423042ebc4e6b9331c00ed93525f1d
an8ar <annssarr@gmail.com>
2024-06-24 17:08:18 +0500

    add: filter, date range filter, sort cancellations' badges

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import { PostList } from '~/feature/posts';
-import { Sort } from '~/feature/sort';
-import { DateRangeFilter, Filter } from '~/feature/filter';
+import { Sort, SortCancel } from '~/feature/sort';
+import { DateFilterCancel, DateRangeFilter, Filter, FilterCancel } from '~/feature/filter';
 
 export default function Home({
   searchParams,
@@ -9,10 +9,17 @@ export default function Home({
 }) {
   return (
     <main className="flex-1 flex flex-col gap-10">
-      <header className=" border-b py-2 flex gap-4">
-        <Filter />
-        <Sort />
-        <DateRangeFilter />
+      <header className=" border-b py-2 flex flex-col gap-2">
+        <div className="flex gap-4">
+          <Filter />
+          <Sort />
+          <DateRangeFilter />
+        </div>
+        <div className="flex gap-2 ml-2">
+          <FilterCancel />
+          <SortCancel />
+          <DateFilterCancel />
+        </div>
       </header>
       <section className=" ">
         <PostList searchParams={searchParams} />
--- src/feature/filter/index.ts
+++ src/feature/filter/index.ts
@@ -1,2 +1,4 @@
 export * from './components/filter';
 export * from './components/date-range-filter';
+export * from './components/filter-cancel';
+export * from './components/date-filter-cancel';
