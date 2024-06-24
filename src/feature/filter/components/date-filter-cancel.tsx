747a4a21d32cec18edb731c02fb33c56c959e20c
an8ar <annssarr@gmail.com>
2024-06-24 17:07:18 +0500

    add: date range filter cancel

---
+++ src/feature/filter/components/date-filter-cancel.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { Badge } from '~/components/ui/badge';
+import { Icon } from '@iconify/react/dist/iconify.js';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+export function DateFilterCancel() {
+  const searchParams = useSearchParams();
+
+  const router = useRouter();
+
+  const pathname = usePathname();
+
+  const params = new URLSearchParams(searchParams.toString());
+
+  const handleClick = () => {
+    if (params.get('date')) {
+      params.delete('date');
+    }
+
+    router.replace(`${pathname}?${params.toString()}`);
+  };
+
+  return (
+    <Badge
+      variant={'secondary'}
+      className="flex gap-1 items-center active:animate-clickAnimation"
+      onClick={handleClick}
+    >
+      Date range <Icon icon="iconoir:cancel" />
+    </Badge>
+  );
+}
