d12a3c9688b4c1a92a8e80f4df00201fa447dd1f
an8ar <annssarr@gmail.com>
2024-06-25 16:24:03 +0500

    add: profile drop down

---
+++ src/feature/profile/components/profile-drop-down.tsx
@@ -0,0 +1,40 @@
+import { User, Globe } from 'lucide-react';
+import { SelectLanguage } from '~/components/select-language';
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '~/components/ui/dropdown-menu';
+
+export function ProfileDropDown() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>AS</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Globe className="mr-2 h-4 w-4" />
+            <SelectLanguage />
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
