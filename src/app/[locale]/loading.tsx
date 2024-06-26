dc0379376cc3ae3e8c0f38cfa5fd61a9b31e11c0
an8ar <annssarr@gmail.com>
2024-06-26 11:07:18 +0500

    refactor: skeleton

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,11 +1,13 @@
 import { Skeleton } from '~/components/ui/skeleton';
 
 export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
   return (
     <div>
-      <Skeleton className="w-full h-[20px]" />
-      Loading...
+      {Array(5)
+        .fill(0)
+        .map((_, index) => (
+          <Skeleton key={index} className="w-full h-36 bg-slate-200 rounded-2xl" />
+        ))}
     </div>
   );
 }
