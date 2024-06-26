bba7b7bc099d536cf0b71683a1c663e8fe74f9a0
an8ar <annssarr@gmail.com>
2024-06-26 15:59:49 +0500

    fix: style of the header

--- src/feature/navigation/components/header.tsx
+++ src/feature/navigation/components/header.tsx
@@ -1,11 +1,9 @@
 'use client';
 import React from 'react';
-import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
 import Image from 'next/image';
 import { Icon } from '@iconify/react';
 import { Button } from '~/components/ui/button';
 import { usePathname, useRouter } from 'next/navigation';
-import { SelectLanguage } from '~/components/select-language';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 import { Message } from './message';
@@ -32,25 +30,41 @@ export function Header({ locale }: Props) {
 
   return (
     <header className=" px-4 py-2 h-14 border-b flex gap-3 items-center justify-between ">
-      <BurgerMenu locale={locale} key={pathname} />
-      <Link href={`/${locale}?sortBy=date&order=asc`} className="flex gap-3 items-center min-w-10">
-        <Image src="/reddit.svg" alt="Reddit" className="min-h-10" width={32} height={32} />
-        <span className="font-extrabold  text-3xl text-orange-600 hidden md:contents">reddit</span>
-      </Link>
-      <HeaderInput />
+      <div className="flex gap-2 items-center">
+        {!!!isDesktop && <BurgerMenu locale={locale} key={`${pathname}-burger-menu`} />}
+
+        <Link
+          href={`/${locale}?sortBy=date&order=asc`}
+          className="flex gap-3 items-center min-w-10"
+        >
+          <Image src="/reddit.svg" alt="Reddit" className="min-h-10" width={32} height={32} />
+          <span className="font-extrabold  text-3xl text-orange-600 hidden md:contents">
+            reddit
+          </span>
+        </Link>
+      </div>
+
+      {isDesktop && <HeaderInput key={`${pathname}-input-desktop`} />}
 
       <nav className="flex gap-3 items-center">
+        {!isDesktop && <HeaderInput key={`${pathname}-input-mobile`} />}
+
         <Message />
 
         <Button
           variant={'link'}
-          className="hover:bg-slate-200 rounded-full flex gap-2 items-center"
+          className="hover:bg-slate-200 rounded-full flex gap-2 items-center p-2"
           onClick={handleClick}
         >
           <Icon icon="teenyicons:add-outline" className="h-5 w-5" />
           <span className="hidden md:contents">{t('button')} </span>
         </Button>
-        {isDesktop ? <ProfileDropDown key={pathname} /> : <ProfileBottomDrawer key={pathname} />}
+
+        {isDesktop ? (
+          <ProfileDropDown key={`${pathname}-dropdown`} />
+        ) : (
+          <ProfileBottomDrawer key={`${pathname}-drawer`} />
+        )}
       </nav>
     </header>
   );
