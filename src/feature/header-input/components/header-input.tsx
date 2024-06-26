48c75569c4b0740e33411d2fb16aaad46d2c0f67
an8ar <annssarr@gmail.com>
2024-06-26 15:59:23 +0500

    add: autocomplete component for posts

--- src/feature/header-input/components/header-input.tsx
+++ src/feature/header-input/components/header-input.tsx
@@ -1,61 +1,10 @@
 'use client';
-import React, { useState, useRef, useEffect } from 'react';
-import { Input } from '~/components/ui/input';
-import { useTranslations } from 'next-intl';
+import React from 'react';
 import { useAppSelector } from '~/store/hooks';
-import { InputPost } from './input-post';
+import { AutoComplete } from './autocomplete';
 
 export function HeaderInput() {
-  const [isVisible, setIsVisible] = useState(false);
-
-  const t = useTranslations('Header');
-
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const dropdownRef = useRef<HTMLDivElement>(null);
-
   const posts = useAppSelector((state) => state.postSlice.posts);
 
-  const handleFocus = () => {
-    setIsVisible(true);
-  };
-
-  const handleClickOutside = (event: MouseEvent) => {
-    const clickedOutsideInput =
-      inputRef.current && !inputRef.current.contains(event.target as Node);
-    const clickedOutsideDropdown =
-      dropdownRef.current && !dropdownRef.current.contains(event.target as Node);
-
-    if (clickedOutsideInput && clickedOutsideDropdown) {
-      setIsVisible(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
-  return (
-    <div className="relative w-full flex flex-1  justify-center min-w-16">
-      <Input
-        ref={inputRef}
-        placeholder={t('input')}
-        className=" bg-slate-200/65 hover:bg-slate-200 relative z-10 rounded-full focus-visible:ring-0 focus-visible:ring-offset-0 text-base"
-        onFocus={handleFocus}
-      />
-      {isVisible && (
-        <div
-          ref={dropdownRef}
-          className="bg-white shadow-lg rounded-lg mt-2 absolute top-0 pt-12 w-full "
-        >
-          {posts.slice(0, 9).map((post) => (
-            <InputPost key={post.id} {...post} setIsVisible={setIsVisible} />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return <AutoComplete posts={posts} />;
 }
