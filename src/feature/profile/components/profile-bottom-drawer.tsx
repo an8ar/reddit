6f74c6da32d16bb9bc979d61e7acbb8c9a08485a
an8ar <annssarr@gmail.com>
2024-06-26 10:42:27 +0500

    refactor: remove control for dialog

--- src/feature/profile/components/profile-bottom-drawer.tsx
+++ src/feature/profile/components/profile-bottom-drawer.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
-import { useParams, useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useParams } from 'next/navigation';
 import { SelectLanguage } from '~/components/select-language';
 import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
 import { Sheet, SheetContent, SheetTitle, SheetTrigger } from '~/components/ui/sheet';
@@ -8,30 +7,24 @@ import { Sheet, SheetContent, SheetTitle, SheetTrigger } from '~/components/ui/s
 export function ProfileBottomDrawer() {
   const { locale } = useParams();
 
-  const [open, setOpen] = useState(false);
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const router = useRouter();
-
   return (
-    <Sheet open={open} onOpenChange={(newOpen) => setOpen(newOpen)}>
+    <Sheet>
       <SheetTrigger asChild>
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>AS</AvatarFallback>
         </Avatar>
       </SheetTrigger>
+
       <SheetContent side={'bottom'} className=" flex h-1/2 flex-col mt- gap-4 text-sm">
-        <Link href={`/${locale}/profile`} className="flex gap-2 items-center" onClick={handleClose}>
+        <Link href={`/${locale}/profile`} className="flex gap-2 items-center">
           <div className="h-5 w-5 bg-black rounded-full" />
           <div className="flex flex-col gap-1">
             <span className="text-sm">View Profile</span>
             <span className="text-xs ">an8ar</span>
           </div>
         </Link>
+
         <SheetTitle className="hidden" />
 
         <SelectLanguage />
