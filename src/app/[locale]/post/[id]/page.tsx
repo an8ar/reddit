3d1757d03b6f25ebba9b0d6c95e3a0a24b3f87c0
an8ar <annssarr@gmail.com>
2024-06-27 12:48:37 +0500

    refactor: remove console.log

--- src/app/[locale]/post/[id]/page.tsx
+++ src/app/[locale]/post/[id]/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default function Page({ params }: { params: { id: string } }) {
-  console.log(params);
   return (
     <div>
       Page of post with id: <span className="text-slate-600 text-sm">{params.id}</span>
