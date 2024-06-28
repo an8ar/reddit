10e723b99d1059b564933d5df2c164122d51bc0a
an8ar <annssarr@gmail.com>
2024-06-28 15:13:24 +0500

    add: redux provider for testing

---
+++ utils/render-with-providers.tsx
@@ -0,0 +1,27 @@
+import React, { PropsWithChildren } from 'react';
+import { render } from '@testing-library/react';
+import type { RenderOptions } from '@testing-library/react';
+import { Provider } from 'react-redux';
+
+import { AppStore, RootState } from '~/store';
+import { makeStore } from '~/store';
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: Partial<RootState>;
+  store?: AppStore;
+}
+
+export function renderWithProviders(
+  ui: React.ReactElement,
+  extendedRenderOptions: ExtendedRenderOptions = {},
+) {
+  const { preloadedState = {}, store = makeStore(), ...renderOptions } = extendedRenderOptions;
+  const Wrapper = ({ children }: PropsWithChildren) => (
+    <Provider store={store}>{children}</Provider>
+  );
+
+  return {
+    store,
+    ...render(ui, { wrapper: Wrapper, ...renderOptions }),
+  };
+}
