2e057e7ee03ee27d8398b24d030ae588aea77d56
an8ar <annssarr@gmail.com>
2024-06-28 15:15:33 +0500

    add: extent provider with next-intl

--- utils/render-with-providers.tsx
+++ utils/render-with-providers.tsx
@@ -2,22 +2,32 @@ import React, { PropsWithChildren } from 'react';
 import { render } from '@testing-library/react';
 import type { RenderOptions } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { NextIntlClientProvider, IntlConfig } from 'next-intl';
+import { AppStore, RootState, makeStore } from '~/store';
 
-import { AppStore, RootState } from '~/store';
-import { makeStore } from '~/store';
+interface IntlProviderProps extends Omit<IntlConfig, 'locale'> {
+  locale: string;
+}
 
 interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
   preloadedState?: Partial<RootState>;
   store?: AppStore;
+  intlProviderProps?: IntlProviderProps;
 }
 
 export function renderWithProviders(
   ui: React.ReactElement,
-  extendedRenderOptions: ExtendedRenderOptions = {},
+  {
+    preloadedState = {},
+    store = makeStore(),
+    intlProviderProps,
+    ...renderOptions
+  }: ExtendedRenderOptions = {},
 ) {
-  const { preloadedState = {}, store = makeStore(), ...renderOptions } = extendedRenderOptions;
   const Wrapper = ({ children }: PropsWithChildren) => (
-    <Provider store={store}>{children}</Provider>
+    <Provider store={store}>
+      <NextIntlClientProvider {...intlProviderProps}>{children}</NextIntlClientProvider>
+    </Provider>
   );
 
   return {
