8774415853a0151997bad05136fce2696369d649
an8ar <annssarr@gmail.com>
2024-07-01 18:01:22 +0500

    add: navigation to feeds

--- cypress/e2e/homepage.cy.tsx
+++ cypress/e2e/homepage.cy.tsx
@@ -1,12 +1,27 @@
 describe('HomePage', () => {
   beforeEach(() => {
     cy.visit('/');
+
+    cy.get('header').should('be.visible');
+
+    cy.get('nav').should('be.visible');
+
+    cy.get('aside').should('be.visible');
   });
 
   it('should load the homepage correctly', () => {
-    cy.get('header').should('be.visible');
-    cy.get('nav').should('be.visible');
-    cy.get('aside').should('be.visible');
     cy.get('section').should('be.visible').should('not.be.null');
+  });
+
+  it('should navigate to popular page', () => {
+    cy.get('a[href*="popular"]').click();
+
+    cy.contains('popular');
+  });
+
+  it('should navigate to all page', () => {
+    cy.get('a[href*="all"]').click();
+
+    cy.contains('all');
   });
 });
