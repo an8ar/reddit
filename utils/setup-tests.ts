3d8a67856b89092ef83237e0d695407379e2950a
an8ar <annssarr@gmail.com>
2024-07-01 14:07:42 +0500

    add: overwrite to work with jsdom with radix-ui portal

---
+++ utils/setup-tests.ts
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom';
+
+class MockPointerEvent extends Event {
+  button: number;
+  ctrlKey: boolean;
+  pointerType: string;
+
+  constructor(type: string, props: PointerEventInit) {
+    super(type, props);
+    this.button = props.button || 0;
+    this.ctrlKey = props.ctrlKey || false;
+    this.pointerType = props.pointerType || 'mouse';
+  }
+}
+
+window.PointerEvent = MockPointerEvent as any;
