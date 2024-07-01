c796df9f3c7f3ab03c6194e643975b31dd495715
an8ar <annssarr@gmail.com>
2024-07-01 12:25:30 +0500

    fix: post form test with rendering different locale, submission steps with dispatching actions

--- __tests__/post-form.test.tsx
+++ __tests__/post-form.test.tsx
@@ -1,59 +1,109 @@
-import { fireEvent, screen, act } from '@testing-library/react';
-import { renderWithProviders } from '../utils/render-with-providers'; // Adjust the import to match your file structure
+import React from 'react';
+import { screen, fireEvent, act } from '@testing-library/react';
+import { renderWithProviders } from '../utils/render-with-providers';
 import { PostForm } from '~/feature/posts/components/post-form';
-import reducer, { addPost } from '~/feature/posts/posts-slice';
-import enMessage from '../messages/en.json';
-jest.mock('next/navigation');
+import enMessages from '../messages/en.json';
+import ruMessages from '../messages/ru.json';
+import '@testing-library/jest-dom';
+import { useRouter } from 'next/navigation';
+import { makeStore } from '~/store';
 
-test('post form rendering', () => {
-  const { container } = renderWithProviders(<PostForm type="image" />, {
-    intlProviderProps: {
-      locale: 'en',
-      timeZone: 'America/New_York',
-      messages: enMessage,
-      now: new Date(),
-      defaultTranslationValues: {
-        i: (chunks: React.ReactNode) => <i>{chunks}</i>,
-      },
-    },
+jest.mock('next/navigation', () => ({
+  __esModule: true,
+  useRouter: jest.fn(),
+}));
+
+const mockRouter = {
+  push: jest.fn(),
+};
+
+describe('PostForm Component', () => {
+  beforeEach(() => {
+    (useRouter as jest.Mock).mockReturnValue(mockRouter);
   });
 
-  expect(container).toMatchSnapshot();
-});
-
-test('should dispatch addPost and navigate on form submit', async () => {
-  const pushMock = jest.fn();
-
-  jest.mock('next/navigation', () => ({
-    useRouter() {
-      return {
-        push: pushMock,
-      };
-    },
-  }));
-
-  jest.mock('~/feature/posts/posts-slice', () => ({
-    addPost: jest.fn(),
-  }));
-
-  renderWithProviders(<PostForm type="image" />, {
-    intlProviderProps: {
-      locale: 'en',
-      timeZone: 'America/New_York',
-      messages: enMessage,
-      now: new Date(),
-      defaultTranslationValues: {
-        i: (chunks: React.ReactNode) => <i>{chunks}</i>,
+  test('renders correctly with initial content', () => {
+    const { container } = renderWithProviders(<PostForm type="image" />, {
+      intlProviderProps: {
+        locale: 'en',
+        messages: enMessages,
+        timeZone: 'America/New_York',
+        now: new Date(),
       },
-    },
+    });
+    expect(screen.getByPlaceholderText('Title')).toBeInTheDocument();
+
+    expect(screen.getByRole('button', { name: 'Post' })).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
   });
 
-  await act(async () => {
-    fireEvent.change(screen.getByPlaceholderText('Title'), {
-      target: { value: 'Test Title' },
+  test('renders correctly with different locale', () => {
+    const { container } = renderWithProviders(<PostForm type="image" />, {
+      intlProviderProps: {
+        locale: 'ru',
+        messages: ruMessages,
+        timeZone: 'America/New_York',
+        now: new Date(),
+      },
     });
 
-    fireEvent.submit(screen.getByRole('button', { name: /Post/i }));
-    expect(reducer(undefined, addPost('image', [], 'Test Title')));
+    expect(screen.getByPlaceholderText('Заголовок')).toBeInTheDocument();
+
+    expect(screen.getByRole('button', { name: 'Добавить' })).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test('handles form submission', async () => {
+    const store = makeStore();
+
+    store.dispatch = jest.fn();
+
+    await act(async () => {
+      renderWithProviders(<PostForm type="image" />, {
+        store,
+        intlProviderProps: {
+          locale: 'en',
+          messages: enMessages,
+          timeZone: 'America/New_York',
+          now: new Date(),
+        },
+      });
+    });
+
+    const expectedUpdateFormPayload = {
+      title: 'Test Title',
+    };
+
+    const expectedAddPostPayload = {
+      id: expect.any(String),
+      createdAt: expect.any(String),
+      title: 'Test Title',
+      imageUrls: [],
+      voteCount: 0,
+    };
+
+    await act(async () => {
+      fireEvent.change(screen.getByPlaceholderText('Title'), {
+        target: { value: 'Test Title' },
+      });
+    });
+
+    expect(store.dispatch).toHaveBeenCalledWith({
+      type: 'postSlice/updateForm',
+      payload: expectedUpdateFormPayload,
+    });
+
+    await act(async () => {
+      fireEvent.submit(screen.getByRole('button', { name: 'Post' }));
+    });
+
+    expect(store.dispatch).toHaveBeenCalledWith({
+      type: 'postSlice/addPost',
+      payload: expectedAddPostPayload,
+    });
+
+    expect(mockRouter.push).toHaveBeenCalledWith('/');
   });
 });
