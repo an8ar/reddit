4289dd082ac3fc50098d49af9b685c4dfe862df2
an8ar <annssarr@gmail.com>
2024-07-02 12:07:28 +0500

    remove: component testing with cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,9 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
-    baseUrl: "http://localhost:3000",
+    baseUrl: 'http://localhost:3000',
     supportFile: false,
-  },
-
-  component: {
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
-    },
   },
 });
