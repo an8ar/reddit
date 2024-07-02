914432d68dff1488bf8a06179d33b156fce7db1a
an8ar <annssarr@gmail.com>
2024-07-02 12:36:17 +0500

    add: form submission with isText

--- cypress/e2e/formpage.cy.tsx
+++ cypress/e2e/formpage.cy.tsx
@@ -1,8 +1,16 @@
 describe('FormPage', () => {
-  beforeEach(() => {});
-  it('Go to the create post page', () => {
+  beforeEach(() => {
     cy.visit(`/en/create-post`);
-
+  });
+  it('Go to the create post page', () => {
     cy.get('h1').contains('Create Post');
+  });
+  it('Check form case: isText', () => {
+    cy.get('textarea[name="title"]').type('ryan');
+    cy.get('textarea[name="text"]').type('gosling');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('not.include', 'create-post');
+    cy.contains('ryan');
+    cy.contains('gosling');
   });
 });
