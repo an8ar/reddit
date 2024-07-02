d1fecac013c8426bfe27de51514b7b7498bc2b77
an8ar <annssarr@gmail.com>
2024-07-02 12:57:14 +0500

    add: validation for the input case with name title

--- cypress/e2e/formpage.cy.tsx
+++ cypress/e2e/formpage.cy.tsx
@@ -2,15 +2,25 @@ describe('FormPage', () => {
   beforeEach(() => {
     cy.visit(`/en/create-post`);
   });
+
   it('Go to the create post page', () => {
     cy.get('h1').contains('Create Post');
   });
-  it('Check form case: isText', () => {
+
+  it('Check form for case: isText', () => {
     cy.get('textarea[name="title"]').type('ryan');
     cy.get('textarea[name="text"]').type('gosling');
     cy.get('button[type="submit"]').click();
     cy.url().should('not.include', 'create-post');
     cy.contains('ryan');
     cy.contains('gosling');
+  });
+
+  it('Validate form input, name = title', () => {
+    cy.get('button[type="submit"]').click();
+
+    cy.get('textarea[name="title"]').should('have.focus');
+
+    cy.get('p').contains('Please fill out this field.').should('be.visible');
   });
 });
